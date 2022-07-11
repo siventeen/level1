1e0b8575c7cc64936eb8e7e85db05fc9e2bbbdcf
wangshiqi <wangshiq@yealink.com>
2022-07-11 11:05:14 +0800

    初始化

---
+++ src/main/java/com/example/level1/Level1Application.java
@@ -0,0 +1,14 @@
+package com.example.level1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level1Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(Level1Application.class, args);
+    }
+
+}
